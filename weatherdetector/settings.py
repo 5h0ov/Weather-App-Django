cbb0d1e9904c5e899171a40c6c55ac1213eff883
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2023-11-01 08:55:48 +0530

    Update settings.py

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1tv0uznfplsmj+#%g#s%c-!vinvk#ug3y8nyqcy)%k_opgq%&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
